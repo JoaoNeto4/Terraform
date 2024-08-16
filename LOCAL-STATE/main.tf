f7bef9b699c6e6e860e13eb5d08bbab03d82df59
JoaoNeto4 <joaoquatlo@hotmail.com>
2024-08-16 19:38:32 -0300

    state

---
+++ LOCAL-STATE/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  #https://developer.hashicorp.com/terraform/language/state
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.60.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+
+  default_tags {
+    tags = {
+      owner      = "joaoneto"
+      managed-by = "terraform"
+    }
+  }
+}
