725381f168a3f04ab7c85be4e0ec07a71f09c850
JoaoNeto4 <joaoquatlo@hotmail.com>
2024-08-22 16:38:18 -0300

    create VM Azure with VNET

---
+++ AZURE-VM-COM-VNET-REMOTE-STATE/main.tf
@@ -0,0 +1,40 @@
+#https://developer.hashicorp.com/terraform/language/functions/file
+#https://developer.hashicorp.com/terraform/language/settings/backends/azurerm#data-source-configuration
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.49.0"
+    }
+  }
+
+  backend "azurerm" {
+    /*
+      resource_group_name name of the previous module used in AWS-AZURE-REMOTE-STATE
+      storage_account_name name of the previous module used in AWS-AZURE-REMOTE-STATE
+      container_name name of the previous module used in AWS-AZURE-REMOTE-STATE
+      directory/name_tfstate
+    */
+    resource_group_name  = "rg-terraform-state"
+    storage_account_name = "joaonetoterraformstate"
+    container_name       = "remote-state"
+    key                  = "azure-vm/terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+data "terraform_remote_state" "vnet" {
+  #key created in AZURE-VNET
+  backend = "azurerm"
+  config = {
+    resource_group_name  = "rg-terraform-state"
+    storage_account_name = "joaonetoterraformstate"
+    container_name       = "remote-state"
+    key                  = "azure-vnet/terraform.tfstate"
+  }
+}
