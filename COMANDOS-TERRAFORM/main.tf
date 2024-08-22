d0e0c34012f3d1a675bc3db4edb34e6f698fa989
JoaoNeto4 <joaoquatlo@hotmail.com>
2024-08-22 19:01:37 -0300

    tests cli command

---
+++ COMANDOS-TERRAFORM/main.tf
@@ -0,0 +1,36 @@
+#https://developer.hashicorp.com/terraform/cli/commands/state
+#https://developer.hashicorp.com/terraform/cli/commands/show
+#https://developer.hashicorp.com/terraform/cli/commands/import
+#https://developer.hashicorp.com/terraform/cli/import/usage
+#https://developer.hashicorp.com/terraform/cli/commands/init
+#https://developer.hashicorp.com/terraform/cli/commands/force-unlock
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.60.0"
+    }
+  }
+
+  backend "s3" {
+    #for automation in pipelines review class 40 chapter 5
+    #backend-dev.hcl
+    #backend-prod.hcl
+    bucket = "joaoneto-remote-state"
+    key    = "commands/terraform.tfstate"
+    region = "us-west-2"
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+
+  default_tags {
+    tags = {
+      owner      = "joaoneto"
+      managed-by = "terraform"
+    }
+  }
+}
