765f9eebfa1e128282bad8535868a38e51992512
JoaoNeto4 <joaoquatlo@hotmail.com>
2024-09-02 17:48:16 -0300

    terraform_data and null_resourse

---
+++ TERRAFORM-DATA-AZURE-VM/main.tf
@@ -0,0 +1,43 @@
+#https://developer.hashicorp.com/terraform/language/functions/file
+#https://developer.hashicorp.com/terraform/language/settings/backends/azurerm#data-source-configuration
+
+#https://registry.terraform.io/providers/hashicorp/null/latest/docs/resources/resource
+#https://developer.hashicorp.com/terraform/language/resources/terraform-data
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.49.0"
+    }
+  }
+
+  backend "azurerm" {
+    /*
+      resource_group_name name of the previous module used in AWS-AZURE-REMOTE-STATE
+      storage_account_name name of the previous module used in AWS-AZURE-REMOTE-STATE
+      container_name name of the previous module used in AWS-AZURE-REMOTE-STATE
+      directory/name_tfstate
+    */
+    resource_group_name  = "rg-terraform-state"
+    storage_account_name = "joaonetoterraformstate"
+    container_name       = "remote-state"
+    key                  = "terraform-data/terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+data "terraform_remote_state" "vnet" {
+  #key created in AZURE-VNET
+  backend = "azurerm"
+  config = {
+    resource_group_name  = "rg-terraform-state"
+    storage_account_name = "joaonetoterraformstate"
+    container_name       = "remote-state"
+    key                  = "azure-vnet/terraform.tfstate"
+  }
+}
