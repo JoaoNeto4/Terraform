e898cabcedb0feaca7e47007635f65546a54a921
JoaoNeto4 <joaoquatlo@hotmail.com>
2024-09-02 16:47:11 -0300

    create VM with time_sleep

---
+++ TIME-SLEEP-AWS-VM/main.tf
@@ -0,0 +1,43 @@
+#https://developer.hashicorp.com/terraform/language/functions/file
+#https://developer.hashicorp.com/terraform/language/settings/backends/s3#data-source-configuration
+
+#https://registry.terraform.io/providers/hashicorp/time/latest/docs/resources/sleep.html
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.60.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "joaoneto-remote-state"
+    key    = "time-sleep/terraform.tfstate"
+    region = "us-west-2"
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+
+  default_tags {
+    tags = {
+      owner      = "joaoneto"
+      managed-by = "terraform"
+    }
+  }
+}
+
+data "terraform_remote_state" "vpc" {
+  /*
+    'key' resource name of previous module used in AWS-VPC 
+  */
+  backend = "s3"
+  config = {
+    bucket = "joaoneto-remote-state"
+    key    = "aws-vpc/terraform.tfstate"
+    region = "us-west-2"
+  }
+}
